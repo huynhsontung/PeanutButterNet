973e5cfb9e0447ef491f9ca44f44aa33adce4e48
Tung Huynh <tung75605@gmail.com>
2021-04-18 01:46:18 -0700

    add training code

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ from tensorflow import keras
 from utils import generate_recursive_indices
 from model import Block, Cell, Operation, OperationType
 import numpy as np
+import tensorflow as tf
+import datetime
 
 
 hparams = {
     'num_conv_filters': 32,
     'num_blocks': 5,
-    'num_cells': 10,
+    'num_cells': 18,
     'num_reduction_cells': 2,
     'dropout_prob': 0.5,
     'filter_scaling_rate': 1.0,
@@ -48,7 +50,7 @@ def generate_cell(reduce=False) -> Cell:
 
 
 def generate_model(input_shape: tuple[int, int, int],
-                   num_classes: int, hparams: dict[str, int]) -> keras.Model:
+                   num_classes: int, hparams: dict[str, int], preprocessing: list[keras.layers.Layer] = [], rescaling=True) -> keras.Model:
     num_cells = hparams['num_cells']
     num_reduction_cells = hparams['num_reduction_cells']
     dropout_prob = hparams['dropout_prob']
@@ -67,7 +69,8 @@ def generate_model(input_shape: tuple[int, int, int],
 
     input_indices = generate_recursive_indices(1, 2, num_cells)
     inputs = keras.Input(input_shape)
-    hiddens = [inputs]
+    x = keras.Sequential(preprocessing)(inputs)
+    hiddens = [x]
 
     for i, cell in enumerate(cells):
         input_tup = input_indices[i]
@@ -88,5 +91,61 @@ def generate_model(input_shape: tuple[int, int, int],
     return model
 
 
-model = generate_model((256, 256, 1), 10, hparams)
-model.summary()
+if __name__ == "__main__":
+    (x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
+    image_shape = tuple(x_train.shape[1:])
+    num_classes = 10
+
+    x_val = x_train[-10000:]
+    y_val = y_train[-10000:]
+    x_train = x_train[:-10000]
+    y_train = y_train[:-10000]
+
+    train_ds = tf.data.Dataset.from_tensor_slices((x_train, y_train))
+    train_ds.shuffle(len(x_train))
+    val_ds = tf.data.Dataset.from_tensor_slices((x_val, y_val))
+
+    preprocessors = [
+        keras.layers.experimental.preprocessing.RandomFlip("horizontal"),
+        keras.layers.experimental.preprocessing.RandomRotation(0.1),
+        keras.layers.experimental.preprocessing.Rescaling(1. / 255)
+    ]
+
+    model = generate_model(
+        image_shape,
+        num_classes,
+        hparams,
+        preprocessing=preprocessors)
+    model.summary()
+
+    model.compile(optimizer='adam',
+                  loss=keras.losses.SparseCategoricalCrossentropy(
+                      from_logits=True),
+                  metrics=['accuracy'])
+
+    log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    tensorboard_cb = keras.callbacks.TensorBoard(
+        log_dir=log_dir, histogram_freq=1)
+    checkpoint_cb = keras.callbacks.ModelCheckpoint(
+        "cifar10_classification.h5", save_best_only=True
+    )
+    early_stopping_cb = keras.callbacks.EarlyStopping(
+        monitor="val_acc", patience=15)
+
+    callbacks = [tensorboard_cb, checkpoint_cb, early_stopping_cb]
+    epochs = 50
+    model.fit(
+        train_ds,
+        epochs=epochs,
+        callbacks=callbacks,
+        validation_data=val_ds)
+
+    predictions = model.predict(x_test)
+    comparision = tf.math.equal(predictions, y_test)
+    comparision = tf.squeeze(comparision)
+    test_acc = tf.reduce_sum(tf.cast(comparision, dtype=tf.float32)).numpy()
+    test_acc = test_acc / len(y_test)
+    print("Config:")
+    print(hparams)
+    print("Log dir: " + log_dir)
+    print("Test accuracy is %.5f percent." % test_acc)
